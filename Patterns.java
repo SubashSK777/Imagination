2ea9e24355fd0f2f47178c93f6a37710a684d0b5
Subash Kumar <subashsk11831@gmail.com>
2025-01-05 15:09:10 +0530

    2025-01-05T09:39:10.686Z:

---
+++ Patterns.java
@@ -0,0 +1,17 @@
+public class Patterns {
+  public static void Triangle (int n) {
+    for (int i = 0; i <= n; i++){
+      for (int j = 0; j <= i; j++){
+        System.out.print(" * ");
+      } System.out.println();
+    }
+
+  public static void NumberTriangle (int n) {
+    for (int i = 0;)
+  }
+
+  }
+  public static void main(String[] args) {
+    Triangle(7);
+  }
+}
