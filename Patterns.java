f1df2bc920d9461e1ad81cf6a10bee89008b7717
Subash Kumar <subashsk11831@gmail.com>
2025-01-06 14:35:57 +0530

    2025-01-06T09:05:57.674Z:

--- Patterns.java
+++ Patterns.java
@@ -10,7 +10,7 @@ public class Patterns {
   public static void UpArrow(int n){
     for (int i = 0; i <= n; i++) {
       for (int j = 0; j <= n; j++) {
-        if (j == n/2 ) {
+        if (j == n/2 || i == n/2) {
           System.out.print(" * ");
         } else {
           System.out.print("   ");
@@ -22,6 +22,6 @@ public class Patterns {
   }
   public static void main(String[] args) {
     // Triangle(7);
-    UpArrow(7);
+    UpArrow(6);
   }
 }
