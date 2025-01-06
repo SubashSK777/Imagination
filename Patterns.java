6eb112bfdc0f9351d188f4d5993902dcc342b2db
Subash Kumar <subashsk11831@gmail.com>
2025-01-06 08:57:12 +0000

    Jan 6, 2025, 2:27 PM

--- Patterns.java
+++ Patterns.java
@@ -10,6 +10,7 @@ public class Patterns {
   public static void UpArrow(int n){
     for (int i = 0; i <= n; i++) {
       for (int j = 0; j <= n; j++) {
+        
         System.out.print(" * ");
       }System.out.println();
     }
