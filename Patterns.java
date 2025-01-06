1137bdb12e916034f444a6dc9ad507de7cd90e20
Subash Kumar <subashsk11831@gmail.com>
2025-01-06 14:47:52 +0530

    2025-01-06T09:17:52.526Z:

--- Patterns.java
+++ Patterns.java
@@ -11,9 +11,9 @@ public class Patterns {
     for (int i = 0; i <= n; i++) {
       for (int j = 0; j <= n; j++) {
         if (j == n/2 || i == n/2) {
-          System.out.print(" * ");
+          System.out.print(" *");
         } else {
-          System.out.print("   ");
+          System.out.print("  ");
         }
       }System.out.println();
     }
