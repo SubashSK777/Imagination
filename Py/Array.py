563b67a8d2dfc6d194918954f1e731c1570933d9
Subash Kumar <subashsk11831@gmail.com>
2025-01-08 15:50:19 +0530

    2025-01-08T10:20:19.315Z:

--- Py/Array.py
+++ Py/Array.py
@@ -1,36 +1,32 @@
 str1 = "ABABABAB"
 str2 = "ABA"
-
-if len(str1) > len(str2):
-  base = str2
-  maxi = str1
-  mini = str2
-else:
-  base = str1
-  maxi = str2
-  mini = str1
-  
-nmax = len(maxi)
-nmini = len(mini)
-print("..............")
-count = 0
-while (len(base) > 0):
-  if count == 1 :
-    break
-  if len(base) == 1:
-    count += 1
-  print(len(base))
-  if (nmax % len(base) == 0 and nmini % len(base) == 0):
-    remax = nmax // len(base)
-    remini = nmini // len(base)
-    
-    if (base * remax == maxi and base * remini == mini):
-      return (base)
-      
+def returning(str1 , str2):
+  if len(str1) > len(str2):
+    base = str2
+    maxi = str1
+    mini = str2
   else:
+    base = str1
+    maxi = str2
+    mini = str1
     
-    base = base.removesuffix(base[-1])
-  
-    
-
-print("Errorrrrrrrrrrr")
+  nmax = len(maxi)
+  nmini = len(mini)
+  print("..............")
+  count = 0
+  while (len(base) > 0):
+    if count == 1 :
+      break
+    if len(base) == 1:
+      count += 1
+    print(len(base))
+    if (nmax % len(base) == 0 and nmini % len(base) == 0):
+      remax = nmax // len(base)
+      remini = nmini // len(base)
+      
+      if (base * remax == maxi and base * remini == mini):
+        return (base)
+        
+    else:    
+      base = base.removesuffix(base[-1])
+  return ""
