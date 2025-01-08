ee22174a8657212f7c3d83ed3dd9ed05b086787a
Subash Kumar <subashsk11831@gmail.com>
2025-01-08 10:31:15 +0530

    2025-01-08T05:01:15.040Z:

--- Py/Array.py
+++ Py/Array.py
@@ -1,13 +1,1 @@
-arr = ["a","aba","ababa","aa"]
-
-count = 0
-
-n = len(arr)
-
-for i in range (n):
-  for j in range (n):
-    if arr[i] in arr[j]:
-      count += 1
-      
-print(count)
-  
+arr = [7,1,5,3,6,4]
