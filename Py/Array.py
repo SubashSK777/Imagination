e7bdff7b5f3acf3b748f3592db748268f6a4d728
Subash Kumar <subashsk11831@gmail.com>
2025-01-08 09:36:38 +0530

    2025-01-08T04:06:38.035Z:

--- Py/Array.py
+++ Py/Array.py
@@ -4,12 +4,13 @@ arr = [2, 7, 11, 15]
 
 target = 9
 
-i = 0
-comp = target- arr[i]
-map[arr[i]] = i
+comp = target- arr[0]
+map[arr[0]] = 0
 
 i = 1
 comp = target- arr[i]
 map[arr[i]] = i
+
+
 print (map)
 
