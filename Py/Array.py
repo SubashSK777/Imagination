953e899d80703b697914fd3e40ea8e5bf39798ee
Subash Kumar <subashsk11831@gmail.com>
2025-01-08 15:48:11 +0530

    2025-01-08T10:18:11.668Z:

--- Py/Array.py
+++ Py/Array.py
@@ -15,14 +15,14 @@ nmini = len(mini)
 print("..............")
 count = 0
 while (len(base) > 0):
-  if count == 2 :
+  if count == 1 :
     break
   if len(base) == 1:
-    count
+    count += 1
   print(len(base))
   if (nmax % len(base) == 0 and nmini % len(base) == 0):
-    remax = nmax / len(base)
-    remini = nmini / len(base)
+    remax = nmax // len(base)
+    remini = nmini // len(base)
     
     if (base * remax == maxi and base * remini == mini):
       print(base)
