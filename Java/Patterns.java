ca2044b046fa0b0ec9e34ff53092443174541f27
Subash Kumar <subashsk11831@gmail.com>
2025-01-09 15:52:23 +0530

    2025-01-09T10:22:23.536Z:

--- Java/Patterns.java
+++ Java/Patterns.java
@@ -24,11 +24,15 @@ public class Patterns {
       for (int j = 0; j < 2*i+1; j++) {
         System.out.print(" ");
       } 
-      for (int k = 0; k < n - i - 1; k ++)
+      for (int k = 0; k < n - i - 1; k++) {
+        System.out.print("* ");
+      }
+      System.out.println();
     }
   }
     public static void main(String[] args) {
     // Triangle(7);
-    UpArrow(12, '#');
+    // UpArrow(12, '#');
+    HourGlass(7);
   }
 }
