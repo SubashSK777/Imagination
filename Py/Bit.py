fc6a79891af52c361f7ef05be086ea94630dc4d2
Subash Kumar <subashsk11831@gmail.com>
2025-01-09 10:13:58 +0530

    2025-01-09T04:43:58.644Z:

--- Py/Bit.py
+++ Py/Bit.py
@@ -1,7 +1,8 @@
 n = 7
 
-for i in range (2, n**0.5):
-  if n % i == 0:
+for i in range (2, int(n**0.5) + 1):
+  if n % i != 0:
     print("Prime")
     break
-  print("Not Prime")
+else:
+print("Not Prime")
