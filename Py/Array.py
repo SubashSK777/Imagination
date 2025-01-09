45c1b661ceb85008be7d6a2050aa3c2705255daa
Subash Kumar <subashsk11831@gmail.com>
2025-01-09 06:00:19 +0530

    2025-01-09T00:30:19.601Z:

--- Py/Array.py
+++ Py/Array.py
@@ -1,33 +1,33 @@
-str1 = "ABABABAB"
-str2 = "ABA"
-def returning(str1 , str2):
-  if len(str1) > len(str2):
-    base = str2
-    maxi = str1
-    mini = str2
-  else:
-    base = str1
-    maxi = str2
-    mini = str1
+# str1 = "ABABABAB"
+# str2 = "ABA"
+# def returning(str1 , str2):
+#   if len(str1) > len(str2):
+#     base = str2
+#     maxi = str1
+#     mini = str2
+#   else:
+#     base = str1
+#     maxi = str2
+#     mini = str1
     
-  nmax = len(maxi)
-  nmini = len(mini)
-  count = 0
-  while (len(base) > 0):
-    if count == 1 :
-      break
-    if len(base) == 1:
-      count += 1
-    if (nmax % len(base) == 0 and nmini % len(base) == 0):
-      remax = nmax // len(base)
-      remini = nmini // len(base)   
-      if (base * remax == maxi and base * remini == mini):
-        return (base)
-      else:    
-        base = base.removesuffix(base[-1])
-    else:    
-      base = base.removesuffix(base[-1])
-  return ""
+#   nmax = len(maxi)
+#   nmini = len(mini)
+#   count = 0
+#   while (len(base) > 0):
+#     if count == 1 :
+#       break
+#     if len(base) == 1:
+#       count += 1
+#     if (nmax % len(base) == 0 and nmini % len(base) == 0):
+#       remax = nmax // len(base)
+#       remini = nmini // len(base)   
+#       if (base * remax == maxi and base * remini == mini):
+#         return (base)
+#       else:    
+#         base = base.removesuffix(base[-1])
+#     else:    
+#       base = base.removesuffix(base[-1])
+#   return ""
 
 
-print(returning("abaaba","ab"))
+# print(returning("abaaba","ab"))
--- Py/Binary.py
+++ Py/Binary.py
@@ -1,6 +1,17 @@
 arr = [4, 7, 8, 12, 45, 99]
 
 target = 45
-L = arr[0]
-U = arr[-1]
-Mid = 
+L = 0
+U = (len(arr)) - 1
+Mid = (U + L) // 2
+
+if target == arr[Mid]:
+  print(Mid)
+else:
+  if arr[Mid] > target:
+    R = Mid
+  elif arr[Mid] < target:
+    L = Mid
+  
+
+    
