af33f53b6dc7ec8a44af7c7d2d0b6879203a129c
Subash Kumar <subashsk11831@gmail.com>
2025-01-09 15:51:20 +0530

    2025-01-09T10:21:20.293Z:

--- Java/Patterns.java
+++ Java/Patterns.java
@@ -21,7 +21,10 @@ public class Patterns {
 
   public static void HourGlass (int n) {
     for (int i = 0; i < n; i++){
-      for (int j = 0; j < )
+      for (int j = 0; j < 2*i+1; j++) {
+        System.out.print(" ");
+      } 
+      for (int k = 0; k < n - i - 1; k ++)
     }
   }
     public static void main(String[] args) {
