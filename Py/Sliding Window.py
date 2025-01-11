146deded7c3f86d4e91dfee865857c5d36ff2b94
Subash Kumar <subashsk11831@gmail.com>
2025-01-11 16:41:19 +0530

    2025-01-11T11:11:19.794Z:

--- Py/Sliding Window.py
+++ Py/Sliding Window.py
@@ -1,4 +1,11 @@
+import math
+
+
+str1 = "ABCdABCd"
+str2 = "ABCd"
+
+
 if str1 + str2 != str2 + str1:
     print("")
-max_length = gcd(len(str1), len(str2))
+max_length = math.gcd(len(str1), len(str2))
 print (str1[:max_length])
