643b3a9fe34b84fee7ad856d35aec39accc2119f
SubashSK777 <subashsk11831@gmail.com>
2025-03-14 10:06:55 +0530

    fix(fibo): correct loop condition and decrement counter The loop condition was incorrect (`n>0`), causing an infinite loop.  Also, missing decrement of counter `n`.

--- Euler/Euler#2Fibo.py
+++ Euler/Euler#2Fibo.py
@@ -6,9 +6,9 @@ print(num1 , end= " ")
 print(num2 , end= " ")
 
 
-while n > 0 :
+while n- > 0 :
     temp = num1
     num1 = num2
     num2 = temp + num1
     print(num2, end= " ")
-    
+    n -= 1
