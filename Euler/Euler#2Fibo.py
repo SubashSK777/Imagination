df3aaac1e6babe407dc9e1adb1952892ab7ca6b6
SubashSK777 <subashsk11831@gmail.com>
2025-03-14 10:15:40 +0530

    feat(euler): comment out Fibonacci sequence code

--- Euler/Euler#2Fibo.py
+++ Euler/Euler#2Fibo.py
@@ -1,14 +1,14 @@
-num1  = 0 
-num2 = 1
-n = 5
+# num1  = 0 
+# num2 = 1
+# n = 5
 
-print(num1 , end= " ")
-print(num2 , end= " ")
+# print(num1 , end= " ")
+# print(num2 , end= " ")
 
 
-while n- > 0 :
-    temp = num1
-    num1 = num2
-    num2 = temp + num1
-    print(num2, end= " ")
-    n -= 1
+# while n - 2 > 0 :
+#     temp = num1
+#     num1 = num2
+#     num2 = temp + num1
+#     print(num2, end= " ")
+#     n -= 1
