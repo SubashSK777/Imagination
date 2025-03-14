817988c825f4b57cc3c7f1332bba8e66263feebc
SubashSK777 <subashsk11831@gmail.com>
2025-03-14 08:35:39 +0530

    fix(shift): Correct loop range in shift function The loop range was incorrectly set to `l8` instead of `l`, causing an error. This commit corrects it to `l`.

--- AtCoder/ShiftOnly.py
+++ AtCoder/ShiftOnly.py
@@ -4,8 +4,8 @@ arr = list(map(int, input().split()))
 l = len(arr)
 
 def shift(l, arr):
-    count = 0
-    for i in range(l8):
+    
+    for i in range(l):
         if arr[i] % 2 == 0:
             arr[i] = arr[i]//2
         else:
