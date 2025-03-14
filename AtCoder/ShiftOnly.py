60a89f99955957ce15ce30e135f78041cf5e712f
SubashSK777 <subashsk11831@gmail.com>
2025-03-14 08:36:03 +0530

    refactor: Improve ShiftOnly algorithm The algorithm now counts the number of shifts possible for the given array.

--- AtCoder/ShiftOnly.py
+++ AtCoder/ShiftOnly.py
@@ -2,8 +2,8 @@ n = int(input())
 arr = list(map(int, input().split()))
 
 l = len(arr)
-
-def shift(l, arr):
+count = 0
+def shift(l, arr,):
     
     for i in range(l):
         if arr[i] % 2 == 0:
