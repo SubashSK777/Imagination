2fe6062bfeb6785bd202abfd880367b27dba404d
SubashSK777 <subashsk11831@gmail.com>
2025-03-14 09:16:39 +0530

    feat: update 1 files (AtCoder/ShiftOnly.py) at 03:46

--- AtCoder/ShiftOnly.py
+++ AtCoder/ShiftOnly.py
@@ -3,17 +3,6 @@ arr = list(map(int, input().split()))
 l = len(arr)
 
 count = 0
-def shift(l, arr,count):
-    
-    for i in range(len(arr)):
-        if arr[i] % 2 == 0 and arr[i] != 0:
-            arr[i] = arr[i]//2
 
-    
-        
-        else:
-            return count
-    count += 1
-    shift(l, arr,count)
 
 print(shift(l, arr, count))
